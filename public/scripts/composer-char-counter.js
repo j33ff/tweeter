3c496c0d808ab975845f1f6ac7337b21d608221b
Jeff FitzGerald <j33ff.fitzgerald@gmail.com>
2017-09-19 21:12:23 +0000

    adding red text to counter for when char count above 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,16 @@
 $(document).ready(function(){
  //$("#msgid").html("This is Hello World by JQuery");
-  function handleNegativeValues(value) {
-    // do stuff if the value is negative
-    // select the p tag that holds the length
-    // change text to red
-  }
-
   $('.char-count').keyup(function () {
-  const max = 140;
-  let len = $(this).val().length;
-  if (len >= max) {
-    $('.counter').val(' you have reached the limit');
-  } else {
-    let char = max - len;
-    $('.counter').val(char + ' characters left');
-    console.log(char);
-  }
+    const max = 140;
+    const len = $(this).val().length;
+
+    $(this).parent().find('.counter').html(max - len);
+    // apply class to change color
+    if (len > max) {
+      $(this).parent().find('span').addClass("charLimit");
+    } else {
+      $(this).parent().find('span').removeClass("charLimit");
+    }
   });
 
 });
