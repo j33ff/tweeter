644515fd9406e6f0666196e859097318e362f4b9
Jeff FitzGerald <j33ff.fitzgerald@gmail.com>
2017-09-20 04:03:59 +0000

    working on dynamic tweet functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,27 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(function(){
 
+  function createTweetElement(tweet){
+    var $tweet = createTweetElement(tweetData);
+    $('<article>').append(tweetData);
+    return $tweet;
+  }
+});
+
+createTweetElement(tweetData);
+
+function renderTweets(tweets) {
+  // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+      var tweetsContainer = $('tweeter-feed').html('');
+
+      tweets.forEach(function(tweet){
+      var tweetElement = createTweetElements(tweet);
+      tweetsContainer.append(tweetElement);
+    });
+}
+
+renderTweets(data);
